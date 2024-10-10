895189cd141edad9d55924c03e35854e8a7e118b
Slater <armando.jaimes@pucp.edu.pe>
2024-10-09 21:56:43 -0500

    Añadir el recurso y los microservicios

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { envs } from './config';
 import { Logger } from '@nestjs/common';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function main() {
   const logger = new Logger('Orders-Main');
 
 
-  const app = await NestFactory.create(AppModule);
-  await app.listen(envs.port);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.NATS,
+    options: {
+      servers: envs.sckNatsServers,
+    }
+  });
+
+  await app.listen();
 
   logger.log(`Orders Microservice running on port ${envs.port}`)
 
